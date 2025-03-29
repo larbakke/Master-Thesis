43fa26c4b59f1c19cefc84c3e194aa69564c1a10
larbakke <larsbakke21@gmail.com>
2025-03-29 15:22:41 +0100

    Some dependencies

--- App/TestDJI_SDK/TestDJI_SDK/ViewController.swift
+++ App/TestDJI_SDK/TestDJI_SDK/ViewController.swift
@@ -5,15 +5,37 @@
 //  Created by Lars Bakke on 10/03/2025.
 //
 
-import UIKit
+//import UIKit
+//
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//}
 
-class ViewController: UIViewController {
+import UIKit
+import DJIUXSDK
+
+class ViewController: DUXDefaultLayoutViewController {
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent;
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.contentViewController?.view.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0).isActive = true
+        self.contentViewController?.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0).isActive = true
+        self.contentViewController?.view.setNeedsDisplay()
     }
-
-
+    @IBAction func TestButton(_ sender: Any) {
+        print("Print")
+        NSLog("NSLog")
+    }
+    
 }
-
