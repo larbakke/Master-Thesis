cbde37c5edc060d0e95d36442698a6b2e12c3dd9
larbakke <larsbakke21@gmail.com>
2025-03-29 15:15:40 +0100

    litt mer i app

---
+++ App/TestDJI_SDK/TestDJI_SDK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestDJI_SDK
+//
+//  Created by Lars Bakke on 10/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
