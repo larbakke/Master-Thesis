8b570883e1fb0d2020ea4e05597d5c8ff5372e29
larbakke <larsbakke21@gmail.com>
2025-06-05 18:35:37 +0200

    nodet mer fra macen tydelig vis

--- ComputerVision/utils/toIOS.py
+++ ComputerVision/utils/toIOS.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a trained YOLO11 PyTorch model
-model = YOLO("yolo11x.pt")
+model = YOLO("ComputerVision/runs/detect/train8/weights/best.pt")
 
 # Export the PyTorch model to CoreML INT8 format with NMS layers
 # The imgsz property may be adjusted when you export a trained model
